5870754cfc606612a61f495412b44f78f7d4cb99
Edward Fallon <eddie_fallon@hotmail.com>
2018-04-02 17:35:49 +0100

    Code Clan Student added

--- code_clan_student/specs/CodeclanStudents_specs.rb
+++ code_clan_student/specs/CodeclanStudents_specs.rb
@@ -1,9 +1,39 @@
 require('minitest/autorun')
 require_relative('../CodeClanStudent.rb')
 
-class TestBankAccount < MiniTest::Test
+class TestCodeClanStudent < MiniTest::Test
 
-  # def setup()
-  #     @bank_account = BankAccount.new("John", 500, "business")
-  #     @bank_account2 = BankAccount.new("Ally", 100, "personal")
-  # end
+  def setup()
+      @student = Student.new("Tom", 5)
+
+  end
+
+
+  def test_student_name()
+    assert_equal("Tom", @student.name)
+  end
+
+  def test_student_number()
+    assert_equal(5, @student.number)
+  end
+
+  def test_set_student_name()
+    @student.set_student_name("Jane")
+    assert_equal("Jane", @student.name)
+  end
+
+  def test_set_student_cohort()
+    @student.set_student_cohort(6)
+    assert_equal(6, @student.number)
+  end
+
+  def test_student_talk()
+    assert_equal("I can talk!", @student.student_talk)
+  end
+
+  def test_favorite_language()
+    assert_equal("I love Ruby", @student.language(" Ruby"))
+
+  end
+
+end
