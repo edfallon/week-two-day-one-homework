eb7ee4f11ba76c0685423b8a1abf6bfb1e2c7dc5
Edward Fallon <eddie_fallon@hotmail.com>
2018-04-02 18:27:35 +0100

    set coach name added

--- sports_team/specs/sports_team_specs.rb
+++ sports_team/specs/sports_team_specs.rb
@@ -1,9 +1,30 @@
 require('minitest/autorun')
 require_relative('../sports_team.rb')
 
-class TestBankAccount < MiniTest::Test
+class TestSportsTeam < MiniTest::Test
 
-  # def setup()
-  #     @bank_account = BankAccount.new("John", 500, "business")
-  #     @bank_account2 = BankAccount.new("Ally", 100, "personal")
-  # end
+  def setup()
+      @sports_team = SportsTeam.new("Hawks",["Dave", "Mike", "Gary"] , "Clint Jones")
+
+  end
+
+  def test_team_name
+    assert_equal("Hawks", @sports_team.team_name)
+  end
+
+  def test_players
+
+    assert_equal(["Dave", "Mike", "Gary"], @sports_team.players)
+  end
+
+  def test_coach
+
+    assert_equal("Clint Jones", @sports_team.coach)
+  end
+
+  def test_set_coach_name
+    @sports_team.set_coach("Bob")
+    assert_equal("Bob", @sports_team.coach)
+  end
+
+end
