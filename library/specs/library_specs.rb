547e6a08b53e6847776f7ef4622374ba8a95d67a
Edward Fallon <eddie_fallon@hotmail.com>
2018-04-02 16:00:14 +0100

    files created

---
+++ library/specs/library_specs.rb
@@ -0,0 +1,9 @@
+require('minitest/autorun')
+require_relative('../library.rb')
+
+class TestBankAccount < MiniTest::Test
+
+  # def setup()
+  #     @bank_account = BankAccount.new("John", 500, "business")
+  #     @bank_account2 = BankAccount.new("Ally", 100, "personal")
+  # end
---
+++ sports_team/specs/sports_team_specs.rb
@@ -0,0 +1,9 @@
+require('minitest/autorun')
+require_relative('../sports_team.rb')
+
+class TestBankAccount < MiniTest::Test
+
+  # def setup()
+  #     @bank_account = BankAccount.new("John", 500, "business")
+  #     @bank_account2 = BankAccount.new("Ally", 100, "personal")
+  # end
